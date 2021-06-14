e4907035af64781f0289f65009566437441af6af
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2021-06-14 16:59:13 +0700

    update broadcast

--- client.py
+++ client.py
@@ -1,63 +1,56 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  9 20:11:04 2021
-
-@author: basuki
-"""
 import socket
-import select
 import sys
-from threading import Thread
+import threading
 
+BUFF_SIZE = 65535
 
 #meminta masukan dari pengguna
 ip_chat = '127.0.0.1'
 port_chat = 8082
-user_chat = input('username for chat: ')
+user_chat = input('Username for chat: ')
+
+# terima dari server
+def recv_msg(socket):
+    while True:
+        data = socket.recv(65535)
+        sys.stdout.write(data.decode() + '\n')
 
 #connect ke server dengan ip chat dan user chat
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((ip_chat, port_chat))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((ip_chat, port_chat))
+client_socket.send(user_chat.encode())
 
-#connect ke ftp dengan username ftp dan password ftp
+thread_cli = threading.Thread(target=recv_msg, args=(client_socket,))
+thread_cli.start()
 
+while True:
+    print ("Input Command :")
+    command = input()
 
-#kirim chat serta command
-def send_msg(sock):
-    while True:
+    if command == "BROADCAST":
+        print("Send Message :")
         message = input()
         message = message + " " + user_chat
-        sock.send(message.encode())
+        client_socket.send(message.encode())
         sys.stdout.write('<You> ')
         message = ' '.join(message.split(' ')[:-1])
         sys.stdout.write(message + '\n')
-        sys.stdout.flush()
+        continue
 
-# terima dari server
-def recv_msg(sock):
-    while True:
-        data = sock.recv(2048)
-        sys.stdout.write(data.decode() + '\n')
+    elif command == "PRIVATE":
+        print ("Masuk Private")
+        continue
 
-Thread(target=send_msg, args=(server,)).start()
-Thread(target=recv_msg, args=(server,)).start()
+    elif command == "ADD":
+        print ("Masuk add")
+        continue
 
+    elif command == "FRIENDLIST":
+        print ("Masuk Friendlist")        
+        continue
 
-while True:
-    # antrian baru
-    sockets_list = [server]
-    #mengirim nama user chat ke server
-    server.send(user_chat.encode())
-    
-    # ambil dari antrian
-    read_socket, write_socket, error_socket = select.select(sockets_list, [], [])
-    
-    # Looping untuk menerima dan mengirim pesan
-    for socks in read_socket:
-        if socks == server:
-            recv_msg(socks)
-        else:
-            send_msg(socks)
+    else :
+        print ("Command doenst exist\n")
+        continue
 
-server.close()
+client_socket.close()
